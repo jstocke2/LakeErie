bb9e3785d52f1c722750ca35f0afb6b02ed0d7ad
jstocke2 <jake.stocker@yahoo.com>
2015-03-16 21:07:27 -0400

    Computes the gallons of water contained in Lake Erie

---
+++ lakeErie.py
@@ -0,0 +1,14 @@
+def compute_gallons():
+    squareFeet=9910.0*27878400
+    depth=62.0
+    cubicFeet=squareFeet*depth
+    print cubicFeet, ' in cubic feet'
+    answer=cubicFeet*7.48051948
+    print answer, " in gallons"
+
+compute_gallons()
+
+
+
+
+    
